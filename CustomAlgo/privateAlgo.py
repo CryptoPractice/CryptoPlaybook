1edb842afa6681311135490f5bf50c56f91d392b
CryptoPractice <125538877+CryptoPractice@users.noreply.github.com>
2023-02-16 16:35:37 +0800

    Re-upload of Challenge

---
+++ CustomAlgo/privateAlgo.py
@@ -0,0 +1,34 @@
+from random import randint
+
+
+a = 288260533169915
+p = 1007621497415251
+
+FLAG = b'CryptoCSG{###########}'
+
+def xor(a,b):
+    if a == b:
+        return '0'
+    else:
+        return '1'
+def encrypt_flag(flag):
+    ciphertext = []
+    plaintext = ''.join([bin(i)[2:].zfill(8) for i in flag])
+    prev = '0'
+    for b in plaintext:
+        e = randint(1, p)
+        n = pow(a, e, p)
+        orig_b = b
+        b = xor(prev,b)
+        prev = orig_b
+        if b == '1':
+            ciphertext.append(n)
+        else:
+            n = -n % p
+            ciphertext.append(n)
+    print(plaintext)
+    print(ciphertext)
+    return ciphertext
+
+
+print(encrypt_flag(FLAG))
